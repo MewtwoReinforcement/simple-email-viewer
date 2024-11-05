b84ea6882afb30fad7500197fe799e5ccfa60879
aidanprior <priorlyaidan@yahoo.com>
2024-11-05 11:20:12 -0500

    fix: update webpack.config.js to commonJS syntax

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,36 @@
-import path from "path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-export default {
-  entry: "./client/index.ts",
+module.exports = {
+  entry: './client/index.ts',
   output: {
-    path: path.join(import.meta.dirname, "/dist"),
-    filename: "bundle.js",
-    publicPath: "/",
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
   },
-  mode: "development",
-  plugins: [new HtmlWebpackPlugin({ template: "./client/index.html" })],
+  mode: 'development',
+  plugins: [new HtmlWebpackPlugin({ template: './client/index.html' })],
   devServer: {
     static: {
-      directory: path.resolve(import.meta.dirname, "public"),
+      directory: path.resolve(__dirname, 'public'),
     },
     port: 8080,
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
-        use: "ts-loader",
+        use: 'ts-loader',
       },
       {
         test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: ['.ts', '.tsx', '.js'],
   },
 };
