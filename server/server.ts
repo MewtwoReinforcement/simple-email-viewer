5c7ef83eb3c2f15711d9657d541e093f298364b5
aidanprior <priorlyaidan@yahoo.com>
2024-11-05 11:49:49 -0500

    feat(oauth): basic oauth setup and email message fetching

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,13 @@
-console.log("Hello Server!");
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/oauth', (_req: Request, res: Response) => {
+  res.status(201).send('superb!');
+});
+
+app.listen(3000, () => {
+  console.log('Server listening at port 3000...');
+});
