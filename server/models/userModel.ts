71aeff5153e8032ab59110f4972d108fe8504fb9
Mason Meyer <mason.meyer@gmail.com>
2024-11-05 10:47:58 -0800

    added userModel

---
+++ server/models/userModel.ts
@@ -0,0 +1,16 @@
+import { Schema, Document, model } from 'mongoose';
+import { NextFunction } from 'express';
+
+interface IUser extends Document {
+  email: string;
+  password: string;
+}
+
+const userSchema = new Schema<IUser>({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const User = model<IUser>('User', userSchema);
+
+export default User;
