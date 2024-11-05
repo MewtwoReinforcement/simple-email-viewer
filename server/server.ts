34aff3bcdc4fbff512dc04d01c9ed1f5a88f8759
Austin Cheng <austincheng25@gmail.com>
2024-11-05 12:25:21 -0500

    database setup

--- server/server.ts
+++ server/server.ts
@@ -1,32 +1,44 @@
 import path from 'path';
 import express, { Request, Response } from 'express';
-import cors from 'cors';
+// import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+mongoose
+  .connect(process.env.MONGO_URI as string)
+  .then((result): void => {
+    console.log('DB connected', result);
+  })
+  .catch((err: string) => {
+    console.log('Failed', err);
+  });
 
 const app = express();
 const port: number = Number(process.env.port) || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(cors());
+// app.use(cors());
 
 app.get('/', (req: Request, res: Response): void => {
-    return res.status(200).sendFile(path.resolve(__dirname, '../client/index.html'));
-  });
+  return res
+    .status(200)
+    .sendFile(path.resolve(__dirname, '../client/index.html'));
+});
 
-  app.get('/users', (req: Request, res: Response): void => {
-    return res.status(200).sendFile(path.resolve(__dirname, '../client/index.html'));
-  });
+app.get('/users', (req: Request, res: Response): void => {
+  return res
+    .status(200)
+    .sendFile(path.resolve(__dirname, '../client/index.html'));
+});
 
-  app.post('/users', (req: Request, res: Response): void => {
-    return res.status(200).sendFile(path.resolve(__dirname, '../client/index.html'));
-  });
+app.post('/users', (req: Request, res: Response): void => {
+  return res
+    .status(200)
+    .sendFile(path.resolve(__dirname, '../client/index.html'));
+});
 
-
-
-  app.listen(port, () => {
-    console.log(`Server listening on port:${port}`);
-  });
-
-
-
+app.listen(port, () => {
+  console.log(`Server listening on port:${port}`);
+});
