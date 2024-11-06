d7020431708ce481664e4e96cea49cb954f54b11
RJ McCarthy <rjmccarthy92@gmail.com>
2024-11-05 21:47:01 -0500

    Updates message filter functionality.

--- client/utilities/whiteListedIndbox.ts
+++ client/utilities/whiteListedIndbox.ts
@@ -1,18 +1,36 @@
 const fakeMessage = {
-  id: '123',
+  id: '2',
   threadId: 'thread123',
   labelIds: ['label123'],
   snippet: 'Snippet of Message',
   historyId: 'history123',
   internalDate: 'MM/DD/YYYY',
   payload: {
-    messagePart: {
-      from: 'sender@place.com',
-      to: 'user@user.com',
+    partId: '1',
+    mimeType: 'unknown',
+    filename: 'email.txt',
+    headers: [
+      { name: 'from', value: 'joeBiden@whitehouse.gov' },
+      { name: 'from', value: 'donaldTrump@assholefuckface.gov' },
+    ],
+    body: {
+      attachmentId: '70',
+      size: 10,
+      data: "Hey, It's your pal Joe. Please Vote today...",
     },
+    parts: [],
   },
   sizeEstimate: 10,
   raw: 'etc',
 };
 
-const whiteListed = (message) => {};
+console.log(fakeMessage.payload.headers[0].value);
+
+// get array of all messages
+// iterate through and filter out from: 'value' if found in contacts
+
+const whiteListed = (unfilteredMsgs, contactsArr) => {
+  //set from property to variable
+  const sender = message.payload.headers[0].value;
+  contactsArr.push(sender);
+};
